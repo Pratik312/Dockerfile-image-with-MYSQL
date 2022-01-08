087bf498965b13fc3c163666f8e5940bee2cd508
Pratik312 <54778171+Pratik312@users.noreply.github.com>
2022-01-08 22:12:35 +0530

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,8 +1,8 @@
 CREATE TABLE studentData(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
 
-INSERT INTO studentData values('Pappu', 85697, 'faltan', 9762845912, 'WRTU2006');
-INSERT INTO studentData values('Vicky', 74586, 'Karad', 9822456318, 'PWTI2007');
-INSERT INTO studentData VALUES('Mayur', 2033, 'Pune', 8796452514, 'HJKI8547');
+INSERT INTO studentData values('Ajay', 88621, 'Mumbai', 9368281622, 'ASGDJ8363');
+INSERT INTO studentData values('Varun', 27326, 'Nagpur', 8737269273, 'HSYAT7362');
+INSERT INTO studentData VALUES('Uday', 63723, 'Pune', 7378926334, 'UDUIE8272');
 
 
 
